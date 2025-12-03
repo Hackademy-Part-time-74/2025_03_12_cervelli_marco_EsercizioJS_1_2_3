cb455e768116f5ebeaf729309c42f168edba8738
Marco Cervelli <marco@bitboss.it>
2025-12-03 19:22:14 +0100

    Secondo commit: Completamento Traccia 1 e 2

---
+++ traccia1.js
@@ -0,0 +1,73 @@
+
+
+
+// TRACCIA 1
+
+/* dichiara una variabile num1, e assegnale un numero qualsiasi
+dichiara una variabile num2, e assegnale un numero qualsiasi
+visualizza a schermo "Il valore della variabile num1 e' tot". Fai la stessa cosa per num2 dichiara una variabile chiamata stringa, e assegnale come valore una stringa qualsiasi stampa a schermo "Il valore della variabile stringa e' tot"
+sostituisci il valore della variabile stringa con un qualsiasi altro valore e poi stampalo a schermo. dichiara una costante chiamata PIGRECO, e assegnale un valore di 3.14159265359
+visualizza a schermo "il valore di PIGRECO e' tot"
+prova a cambiare il valore della costante e vedi cosa succede*/
+
+var num1 = 20
+
+var num2 = 10
+
+var stringa = 'variabile stringa'
+
+const PIGRECO = 3.14159265359
+
+console.log("Il valore della variabile num1 è",num1)
+
+console.log("Il valore della variabile num2 è",num2)
+
+console.log("Il valore della variabile stringa è",stringa)
+
+console.log("Il valore della variabile PIGRACO è",PIGRECO)
+
+
+// TRACCIA 2
+
+/* Scrivi un programma che dati 5 numeri restituisca in output la somma e la media dove:
+
+Input: a = 1, b = 2, c = 3, d = 4, e = 5
+Output: La somma è X e la media e Y*/
+
+
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+
+var x = a + b + c + d + e
+
+var y = (a + b + c + d + e) / 5
+
+console.log("La somma dei 5 numeri ha come risultato", x)
+console.log("La media dei 5 numeri ha come risultato", y)
+
+
+
+
+
+
+
+
+
+
+// TRACCIA 3
+/* Scrivi un programma che dato l'anno corrente ed il tuo anno di nascita determini:
+
+l'età della persona
+quanti anni sono necessari per raggiungere i 100
+e restituisca in output entrambi i risultati.
+
+Ad esempio:
+
+input: anno corrente 2023, anno di nascita 1998
+output: hai 25 anni e ti mancano 75 anni per compierne 100*/
+
+
+
